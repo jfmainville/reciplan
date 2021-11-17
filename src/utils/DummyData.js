68a3a390b3f9c18322c034fe89e8e9f28ca14d7d
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-11-16 20:56:35 -0500

    Add the DummyData.js file to the project
    Add the DummyData.js file to the project. This file contains dummy data used to show a pre-made recipes list to validate the design.

---
+++ src/utils/DummyData.js
@@ -0,0 +1,140 @@
+export const dummyRecipes = [
+	{
+		"id": 1,
+		"name": "Carbonara",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	},
+	{
+		"id": 2,
+		"name": "Steak and Fries",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	},
+	{
+		"id": 3,
+		"name": "Fish and Chips",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	},
+	{
+		"id": 4,
+		"name": "Fish and Chips",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	},
+	{
+		"id": 5,
+		"name": "Fish and Chips",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	},
+	{
+		"id": 6,
+		"name": "Fish and Chips",
+		"preparation_time": 10,
+		"cook_time": 40,
+		"ingredients": [
+			{
+				"name": "Pancetta",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Parmesan",
+				"weight_unit": "g",
+				"quantity": 100
+			},
+			{
+				"name": "Egg",
+				"weight_unit": "qty",
+				"quantity": 2
+			}
+		]
+	}
+];
