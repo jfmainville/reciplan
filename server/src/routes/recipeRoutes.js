2d72aef2ed96e4bac6f1d255bfdb0659c004800a
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-12-07 21:33:19 -0500

    Update recipes POST route with the valid attributes
    Update the recipes POST route with the valid attributes.

--- server/src/routes/recipeRoutes.js
+++ server/src/routes/recipeRoutes.js
@@ -15,14 +15,20 @@ router.get("/recipes", async (req, res) => {
 });
 
 router.post("/recipes", async (req, res) => {
-	const { name, locations } = req.body;
+	const { recipeName, recipeStyle, recipePreparationTime, recipeCookTime } = req.body;
 
-	if (!name || !locations) {
-		return res.status(422).send({ error: "You must provide a name and locations" });
+	if (!recipeName || !recipeStyle || !recipePreparationTime || !recipeCookTime) {
+		return res.status(422).send({ error: "You must provide all the recipe information to continue" });
 	}
 
 	try {
-		const recipe = new Recipe({ userId: req.user._id, name, locations });
+		const recipe = new Recipe({
+			name: recipeName,
+			style: recipeStyle,
+			preparationTime: recipePreparationTime,
+			cookTime: recipeCookTime,
+			userId: req.user._id
+		});
 		await recipe.save();
 		res.send(recipe);
 	} catch (err) {
