3b0efa35bff3b1a0943bd3daef95a1e078c7af22
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-12-21 17:30:04 -0500

    Add RecipeUpdateScreen screen to the RecipeDetailScreen
    Add the RecipeUpdateScreen screen to the RecipeDetailScreen.
    
    - Add the fetchRecipes function from the RecipeContext context
    - Add the NavigationEvents component
    - Add the RecipeDetailScreen navigationOptions parameter
    - Update the styles variable

--- app/src/screens/RecipeDetailScreen.js
+++ app/src/screens/RecipeDetailScreen.js
@@ -1,28 +1,53 @@
 import React, { useContext } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Context as RecipeContext } from "../context/RecipeContext";
-import RecipeListScreen from "./RecipeListScreen";
+import { FontAwesome } from "@expo/vector-icons";
+import { NavigationEvents } from "react-navigation";
 
 const RecipeDetailScreen = ({ navigation }) => {
-	const { state } = useContext(RecipeContext);
+	const { state: recipes, fetchRecipes } = useContext(RecipeContext);
 	const id = navigation.getParam("id");
+	const recipe = recipes.filter(recipe => recipe._id === id)[0];
 
 	return (
 		<View>
-			<Text>Recipe Detail Screen -> {id}</Text>
+			<NavigationEvents onWillFocus={fetchRecipes}/>
+			<Text>Recipe Detail Screen -> {recipe._id} -> {recipe.style}</Text>
 		</View>
 	);
 };
 
-RecipeDetailScreen.navigationOptions = {
-	title: "Recipe Details",
-	headerStyle: {
-		backgroundColor: "#4854C7",
-	},
-	headerTintColor: "#fff",
-	headerTitleStyle: {
-		fontWeight: "bold",
-	}
+RecipeDetailScreen.navigationOptions = ({ id, navigation }) => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate("RecipeUpdate", { id: navigation.getParam("id") })}>
+				<FontAwesome style={styles.icon} name="pencil" size={25}/>
+			</TouchableOpacity>
+		),
+		title: "Recipe",
+		headerStyle: {
+			backgroundColor: "#4854C7",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontWeight: "bold",
+		},
+	};
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	card: {
+		flex: 1,
+		height: 80,
+	},
+	icon: {
+		color: "white",
+		fontSize: 24,
+		marginRight: 10
+	}
+});
 
 export default RecipeDetailScreen;
