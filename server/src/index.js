1e1ea56c82d99f955b2e9835f3a4d31a344d26c2
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-12-28 17:08:29 -0500

    Add the ingredients route to the project
    Add the ingredients route to the project. This route is used to define the name for each recipe ingredient.

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 require("./models/User");
 require("./models/Recipe");
+require("./models/Ingredient");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
+const ingredientRoutes = require("./routes/ingredientRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
@@ -12,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(recipeRoutes);
+app.use(ingredientRoutes);
 
 const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri);
---
+++ server/src/routes/ingredientRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middlewares/requireAuth");
+
+const Ingredient = mongoose.model("Ingredient");
+
+const router = express.Router();
+
+// User must be signed in to access these routes
+router.use(requireAuth);
+
+router.post("/ingredients", async (req, res) => {
+	const { ingredientName } = req.body;
+	
+	const ingredient = await Ingredient.find({ name: { $regex: ".*" + ingredientName + ".*", "$options": "i" } });
+	res.send(ingredient);
+});
+
+module.exports = router;
