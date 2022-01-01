b180beccc01255fb9354786ef73844cdd9c2bd9d
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-01-01 12:16:59 -0500

    Add the GroceryContext.js screen to the project
    Add the GroceryContext.js screen to the project. This screen is used to list the ingredients for the selected recipes and other.

---
+++ app/src/screens/GroceryListScreen.js
@@ -0,0 +1,110 @@
+import React, { useContext, useEffect } from "react";
+import { View, FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Context as GroceryContext } from "../context/GroceryContext";
+import { SafeAreaView } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
+import { Button, ListItem } from "react-native-elements";
+
+const GroceryListScreen = ({ navigation }) => {
+	const { state, fetchGroceries } = useContext(GroceryContext);
+
+	let ingredients = [];
+	if (state.length > 0) {
+		state.reduce((result, ingredient) => {
+			if (!result[ingredient.name]) {
+				result[ingredient.name] = {
+					_id: ingredient._id,
+					name: ingredient.name,
+					weightUnit: ingredient.weightUnit,
+					quantity: 0
+				};
+				ingredients.push(result[ingredient.name]);
+			}
+			result[ingredient.name].quantity += ingredient.quantity;
+			return result;
+		}, {});
+	}
+
+	useEffect(() => {
+		fetchGroceries();
+
+		const listener = navigation.addListener("didFocus", () => {
+			fetchGroceries();
+		});
+
+		return () => {
+			listener.remove();
+		};
+	}, []);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				showsHorizontalScrollIndicator={false}
+				showsVerticalScrollIndicator={false}
+				data={ingredients}
+				keyExtractor={(item) => item._id}
+				renderItem={({ item }) => {
+					return (
+						<ListItem.Swipeable
+							leftContent={
+								<Button
+									title="Done"
+									icon={{ name: "check", color: "white" }}
+									buttonStyle={{ minHeight: "100%" }}
+								/>
+							}
+							rightContent={
+								<Button
+									title="Delete"
+									icon={{ name: "delete", color: "white" }}
+									buttonStyle={{ minHeight: "100%", backgroundColor: "red" }}
+								/>
+							}
+						>
+							<View style={styles.card}>
+								<Text style={{ fontSize: 20 }}>{item.quantity}{item.weightUnit} {item.name}</Text>
+							</View>
+						</ListItem.Swipeable>
+					);
+				}}
+			/>
+		</SafeAreaView>
+	);
+};
+
+GroceryListScreen.navigationOptions = ({ navigation }) => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate("GroceryCreate")}>
+				<FontAwesome style={styles.icon} name="plus" size={25}/>
+			</TouchableOpacity>
+		),
+		title: "Groceries",
+		headerStyle: {
+			backgroundColor: "#4854C7",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontWeight: "bold",
+		},
+	};
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	card: {
+		flex: 1,
+		justifyContent: "center",
+		height: 40,
+	},
+	icon: {
+		color: "white",
+		fontSize: 24,
+		marginRight: 10
+	}
+});
+
+export default GroceryListScreen;
