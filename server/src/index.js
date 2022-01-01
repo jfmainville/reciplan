5fd4d291c206d6eef69671d895b56c64cae6df72
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-01-01 11:44:16 -0500

    Add the groceries routes to the project
    Add the groceries routes to the project.

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 require("./models/User");
 require("./models/Recipe");
 require("./models/Ingredient");
+require("./models/Grocery");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
 const ingredientRoutes = require("./routes/ingredientRoutes");
+const groceryRoutes = require("./routes/groceryRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
@@ -15,6 +17,7 @@ app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(recipeRoutes);
 app.use(ingredientRoutes);
+app.use(groceryRoutes);
 
 const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri);
---
+++ server/src/routes/groceryRoutes.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middlewares/requireAuth");
+
+const Grocery = mongoose.model("Grocery");
+
+const router = express.Router();
+
+// User must be signed in to access these routes
+router.use(requireAuth);
+
+router.get("/groceries", async (req, res) => {
+	const groceries = await Grocery.find({ userId: req.user._id });
+	res.send(groceries);
+});
+
+router.post("/groceries/ingredients", async (req, res) => {
+	const { recipe } = req.body;
+
+	const updatedRecipes = recipe.ingredients.map(ingredient => {
+		delete ingredient._id;
+		if (recipe.recipeId) {
+			ingredient.recipeId = recipe.recipeId;
+		} else {
+			ingredient.recipeId = null;
+		}
+		ingredient.userId = req.user._id;
+		return ingredient;
+	});
+
+	if (!recipe || !updatedRecipes) {
+		return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+	}
+
+	try {
+		Grocery.insertMany(updatedRecipes);
+		res.send(updatedRecipes);
+	} catch (err) {
+		return res.status(422).send({ error: err.message });
+	}
+});
+
+router.post("/groceries", async (req, res) => {
+	const { recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients } = req.body;
+
+	if (!recipeName || !recipeStyle || !recipePreparationTime || !recipeCookTime || !ingredients) {
+		return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+	}
+
+	try {
+		const recipe = new Grocery({
+			name: recipeName,
+			style: recipeStyle,
+			preparationTime: recipePreparationTime,
+			cookTime: recipeCookTime,
+			ingredients: ingredients,
+			userId: req.user._id
+		});
+		await recipe.save();
+		res.send(recipe);
+	} catch (err) {
+		return res.status(422).send({ error: err.message });
+	}
+});
+
+router.put("/groceries/update/:id", (req, res) => {
+	const recipeId = req.params.id;
+	const { recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients } = req.body;
+
+	if (!recipeId || !recipeName || !recipeStyle || !recipePreparationTime || !recipeCookTime || !ingredients) {
+		return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+	}
+
+	try {
+		Grocery.findById(recipeId, async (err, recipe) => {
+			if (!recipe)
+				return res.status(422).send({ error: err.message });
+			else {
+				recipe.name = recipeName;
+				recipe.style = recipeStyle;
+				recipe.preparationTime = recipePreparationTime;
+				recipe.cookTime = recipeCookTime;
+				recipe.ingredients = ingredients;
+				recipe.userId = req.user._id;
+
+				await recipe.save();
+			}
+			res.send(recipe);
+		});
+	} catch (err) {
+		return res.status(422).send({ error: err.message });
+	}
+});
+
+router.delete("/groceries/delete/:id", async (req, res) => {
+	const recipeId = req.params.id;
+
+	if (!recipeId) {
+		return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+	}
+
+	try {
+		await Grocery.deleteOne({ _id: recipeId });
+		res.send();
+	} catch (err) {
+		return res.status(422).send({ error: err.message });
+	}
+});
+
+module.exports = router;
