88a57a4ac124bc2bd2308e8f55b87856eca06cb0
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-01-26 20:47:48 -0500

    Refactor the Ingredient model to support multiple languages
    Refactor the Ingredient model to support multiple languages.
    
    - Add the translations field
    - Add the timestamps default fields
    - Remove the category field for now as it's not used by the application

--- server/src/models/Ingredient.js
+++ server/src/models/Ingredient.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 
+const translationSchema = new mongoose.Schema({
+    singular: String,
+    plural: String,
+    languageCode: String
+});
+
 const ingredientSchema = new mongoose.Schema({
-		name: {
-			type: "String",
-			ref: "User"
-		},
-		category: {
-			type: "String",
-			default: ""
-		}
-	}
+        name: {
+            type: "String",
+            required: true
+        },
+        translations: [translationSchema]
+    },
+    {
+        timestamps: true
+    }
 );
 
 mongoose.model("Ingredient", ingredientSchema);
