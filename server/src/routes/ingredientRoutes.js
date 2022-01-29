6750b2804a1ebd27bc0514d575a3a5d630ba8aff
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-01-29 16:11:19 -0500

    Refactor the ingredient definition route to create ingredient each time
    Refactor the ingredient definition route to create ingredient each time. This new route translate the ingredient name to multiple languages (currently french and english) and it pluralize the ingredient name for each language.

--- server/src/routes/ingredientRoutes.js
+++ server/src/routes/ingredientRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const pluralize = require("pluralize");
+const axios = require("axios");
 const requireAuth = require("../middlewares/requireAuth");
 
 const Ingredient = mongoose.model("Ingredient");
@@ -9,11 +11,68 @@ const router = express.Router();
 // User must be signed in to access these routes
 router.use(requireAuth);
 
-router.post("/ingredients", async (req, res) => {
-	const { ingredientName } = req.body;
+router.post("/ingredients/create", async (req, res) => {
+        let { ingredientName } = req.body;
+        ingredientName = ingredientName.toLowerCase()
 
-	const ingredient = await Ingredient.find({ name: { $regex: ingredientName + ".*", "$options": "i" } });
-	res.send(ingredient);
-});
+        if (!ingredientName) {
+            return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+        }
+
+        // Extract the translation for each ingredient
+        const deeplTranslateUrl = `${process.env.DEEPL_URL}/v2/translate?auth_key=${process.env.DEEPL_API_KEY}`
+        const ingredientNameSource = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&source_lang=FR&target_lang=EN`)
+
+        try {
+            Ingredient.findOne({ name: ingredientNameSource["data"]["translations"][0]["text"] }, async (err, doc) => {
+                if (!doc) {
+                    if (ingredientName === ingredientNameSource["data"]["translations"][0]["text"]) {
+                        const { data } = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&source_lang=EN&target_lang=FR`)
+
+                        const ingredient = new Ingredient({
+                            name: ingredientName,
+                            translations: [
+                                {
+                                    singular: ingredientName,
+                                    plural: pluralize(ingredientName),
+                                    languageCode: "EN"
+                                },
+                                {
+                                    singular: data["translations"][0]["text"],
+                                    plural: pluralize(data["translations"][0]["text"]),
+                                    languageCode: "FR"
+                                }
+                            ]
+                        })
+                        await ingredient.save();
+                        res.send(ingredient);
+                    } else {
+                        const { data } = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&target_lang=EN`)
+
+                        const ingredient = new Ingredient({
+                            name: data["translations"][0]["text"],
+                            translations: [
+                                {
+                                    singular: data["translations"][0]["text"],
+                                    plural: pluralize(data["translations"][0]["text"]),
+                                    languageCode: "EN"
+                                },
+                                {
+                                    singular: ingredientName,
+                                    plural: pluralize(ingredientName),
+                                    languageCode: "FR"
+                                }
+                            ]
+                        })
+                        await ingredient.save();
+                        res.send(ingredient);
+                    }
+                }
+            })
+        } catch (err) {
+            return res.status(422).send({ error: err.message });
+        }
+    }
+)
 
 module.exports = router;
