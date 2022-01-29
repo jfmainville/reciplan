813e345c7c6adeabf278527c667e5c0922f6cc17
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-01-29 16:06:08 -0500

    Add the deviceLanguage variable to the signup method
    Add the deviceLanguage variable to the signup method. This variable extracts the device current language configuration to be used by the application.

--- app/src/context/AuthContext.js
+++ app/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform, NativeModules } from 'react-native'
 import createDataContext from "./createDataContext";
 import recipeApi from "../api/recipe";
 import { navigate } from "../navigationRef";
@@ -35,7 +36,12 @@ const clearErrorMessage = (dispatch) => () => {
 
 const signup = (dispatch) => async ({ email, password }) => {
 	try {
-		const response = await recipeApi.post("/signup", { email, password });
+		const deviceLanguage =
+			Platform.OS === 'ios'
+				? NativeModules.SettingsManager.settings.AppleLocale ||
+				NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+				: NativeModules.I18nManager.localeIdentifier;
+		const response = await recipeApi.post("/signup", { email, password, deviceLanguage });
 		await AsyncStorage.setItem("token", response.data.token);
 		dispatch({ type: "signin", payload: response.data.token });
 
