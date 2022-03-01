3fd762be33c34453d6f5d4d0b2b93dd2f961ac44
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-02-28 20:52:47 -0500

    Add the user account delete route to the authRoutes.js routes
    Add the user account delete route to the authRoutes.js routes.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 const User = mongoose.model("User");
+const Recipe = mongoose.model("Recipe");
+const Grocery = mongoose.model("Grocery");
 
 const router = express.Router();
 
@@ -45,6 +47,26 @@ router.post("/signin", async (req, res) => {
 	} catch (err) {
 		return res.status(422).send({ error: "Invalid password or email" });
 	}
+});
+
+router.post("/user/delete", async (req, res) => {
+	const { email } = req.body;
+
+	if (!email) {
+		return res.status(422).send({ error: "Must provide email and password" });
+	}
+
+	const user = await User.findOne({ email });
+
+	try {
+		await Recipe.deleteMany({ userId: user["_id"].toString() })
+		await Grocery.deleteMany({ userId: user["_id"].toString() })
+		await User.deleteOne({ _id: user["_id"].toString() })
+	} catch (err) {
+		return res.status(422).send({ error: err.message });
+	}
+
+	res.send(user)
 });
 
 module.exports = router;
