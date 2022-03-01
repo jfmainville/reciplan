3be7fa5fb1a6fe13cabb69a308c215d1cff0933c
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-02-28 20:49:00 -0500

    Add the delete account button to the AccountScreen.js screen
    Add the delete account button to the AccountScreen.js screen.
    
    - Add the deleteAccount method from the AuthContext.js context
    - Add the delete account button

--- app/src/screens/AccountScreen.js
+++ app/src/screens/AccountScreen.js
@@ -6,13 +6,16 @@ import { Button } from "react-native-elements";
 import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
-	const { signout } = useContext(AuthContext);
+	const { signout, deleteAccount } = useContext(AuthContext);
 
 	return (
 		<SafeAreaView forceInset={{ top: "always" }}>
 			<Text style={{ fontSize: 48 }}>AccountScreen</Text>
 			<Button
 				title="Sign Out" onPress={signout}
+			/>
+			<Button
+				title="DeleteAccount" onPress={deleteAccount}
 			/>
 		</SafeAreaView>
 	);
