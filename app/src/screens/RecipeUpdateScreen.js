d2c8721f202630a0ac43debb42e3a8a8ea4e0abc
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-04 15:39:45 -0500

    Refactor RecipeUpdateScreen.js screen to save the data using the navigation bar
    Refactor the RecipeUpdateScreen.js screen to save the data using the navigation bar.

--- app/src/screens/RecipeUpdateScreen.js
+++ app/src/screens/RecipeUpdateScreen.js
@@ -1,15 +1,23 @@
-import React, { useContext } from "react";
-import { StyleSheet } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Context as RecipeContext } from "../context/RecipeContext";
 import RecipeMultiStepsForm from "../components/RecipeMultiStepsForm";
+import { FontAwesome } from "@expo/vector-icons";
 
 const RecipeUpdateScreen = ({ navigation }) => {
 	const { state: recipes, updateRecipe } = useContext(RecipeContext);
 	const recipeId = navigation.getParam("id");
 	const recipe = recipes.find(recipe => recipe._id === recipeId);
 
+	useEffect(() => {
+		navigation.setParams({
+			updateRecipe
+		});
+	}, []);
+
 	return (
 		<RecipeMultiStepsForm
+			navigation={navigation}
 			initialValues={{
 				_id: recipeId,
 				name: recipe.name,
@@ -22,17 +30,39 @@ const RecipeUpdateScreen = ({ navigation }) => {
 	);
 };
 
-RecipeUpdateScreen.navigationOptions = {
-	title: "Update Recipe",
-	headerStyle: {
-		backgroundColor: "#4854C7",
-	},
-	headerTintColor: "#fff",
-	headerTitleStyle: {
-		fontWeight: "bold",
+RecipeUpdateScreen.navigationOptions = ({ navigation }) => {
+	const updateRecipe = navigation.getParam("updateRecipe")
+	const recipeId = navigation.getParam("id")
+	const recipeName = navigation.getParam("recipeName")
+	const recipeStyle = navigation.getParam("recipeStyle")
+	const recipePreparationTime = navigation.getParam("recipePreparationTime")
+	const recipeCookTime = navigation.getParam("recipeCookTime")
+	const ingredients = navigation.getParam("ingredients")
+	return {
+		headerRight: () => (
+			<View style={{ flexDirection: "row" }}>
+				<TouchableOpacity
+					onPress={() => updateRecipe(recipeId, recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients, () => navigation.navigate("RecipeDetail", { id: recipeId }))}>
+					<FontAwesome style={styles.headerIcons} name="check" size={25}/>
+				</TouchableOpacity>
+			</View>
+		),
+		title: "Update Recipe",
+		headerStyle: {
+			backgroundColor: "#4854C7",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontWeight: "bold",
+		}
 	}
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	headerIcons: {
+		marginRight: 10,
+		color: "#fff"
+	}
+});
 
 export default RecipeUpdateScreen;
