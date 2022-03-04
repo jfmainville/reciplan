7cf4f94d6749583fffc79d30cb93900e0383edd4
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-04 15:29:39 -0500

    Refactor RecipeCreateScreen.js screen to save the data using the navigation bar
    Refactor the RecipeCreateScreen.js screen to save the data using the navigation bar.

--- app/src/screens/RecipeCreateScreen.js
+++ app/src/screens/RecipeCreateScreen.js
@@ -1,29 +1,58 @@
-import React, { useContext } from "react";
-import { StyleSheet } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Context as RecipeContext } from "../context/RecipeContext";
 import RecipeMultiStepsForm from "../components/RecipeMultiStepsForm";
+import { FontAwesome } from "@expo/vector-icons";
 
 const RecipeCreateScreen = ({ navigation }) => {
 	const { createRecipe } = useContext(RecipeContext);
 
+	useEffect(() => {
+		navigation.setParams({
+			createRecipe
+		});
+	}, []);
+
 	return (
 		<RecipeMultiStepsForm
-			onSubmit={(recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients) => createRecipe(recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients, () => navigation.navigate("RecipeList"))}
+			navigation={navigation}
 		/>
 	);
 };
 
-RecipeCreateScreen.navigationOptions = {
-	title: "Create Recipe",
-	headerStyle: {
-		backgroundColor: "#4854C7",
-	},
-	headerTintColor: "#fff",
-	headerTitleStyle: {
-		fontWeight: "bold",
+RecipeCreateScreen.navigationOptions = ({ navigation }) => {
+	const createRecipe = navigation.getParam("createRecipe")
+	const recipeName = navigation.getParam("recipeName")
+	const recipeStyle = navigation.getParam("recipeStyle")
+	const recipePreparationTime = navigation.getParam("recipePreparationTime")
+	const recipeCookTime = navigation.getParam("recipeCookTime")
+	const ingredients = navigation.getParam("ingredients")
+
+	return {
+		headerRight: () => (
+			<View style={{ flexDirection: "row" }}>
+				<TouchableOpacity
+					onPress={() => createRecipe(recipeName, recipeStyle, recipePreparationTime, recipeCookTime, ingredients, () => navigation.navigate("RecipeList"))}>
+					<FontAwesome style={styles.headerIcons} name="check" size={25}/>
+				</TouchableOpacity>
+			</View>
+		),
+		title: "Create Recipe",
+		headerStyle: {
+			backgroundColor: "#4854C7",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontWeight: "bold",
+		}
 	}
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	headerIcons: {
+		marginRight: 10,
+		color: "#fff"
+	}
+});
 
 export default RecipeCreateScreen;
