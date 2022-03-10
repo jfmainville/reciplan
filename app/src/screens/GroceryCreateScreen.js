ee13ed54f66bf8556ab7c8c814b75ae1e9af04bb
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-09 20:18:09 -0500

    Refactor GroceryCreateScreen.js screen to use the navigationOptions parameter
    Refactor the GroceryCreateScreen.js screen to use the navigationOptions parameter.

--- app/src/screens/GroceryCreateScreen.js
+++ app/src/screens/GroceryCreateScreen.js
@@ -1,29 +1,55 @@
-import React, { useContext } from "react";
-import { StyleSheet } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import GroceryForm from "../components/GroceryForm";
 import { Context as GroceryContext } from "../context/GroceryContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const GroceryCreateScreen = ({ navigation }) => {
 	const { createGrocery } = useContext(GroceryContext);
 
+	useEffect(() => {
+		navigation.setParams({
+			createGrocery
+		});
+	}, []);
+
 	return (
 		<GroceryForm
-			onSubmit={(groceryQuantity, groceryWeightUnit, groceryName) => createGrocery(groceryQuantity, groceryWeightUnit, groceryName, () => navigation.navigate("GroceryList"))}
+			navigation={navigation}
 		/>
 	);
 };
 
-GroceryCreateScreen.navigationOptions = {
-	title: "Add Grocery Item",
-	headerStyle: {
-		backgroundColor: "#4854C7",
-	},
-	headerTintColor: "#fff",
-	headerTitleStyle: {
-		fontWeight: "bold",
+GroceryCreateScreen.navigationOptions = ({ navigation }) => {
+	const createGrocery = navigation.getParam("createGrocery")
+	const groceryQuantity = navigation.getParam("groceryQuantity")
+	const groceryName = navigation.getParam("groceryName")
+
+	return {
+		headerRight: () => (
+			<View style={{ flexDirection: "row" }}>
+				<TouchableOpacity
+					onPress={() => createGrocery(groceryQuantity, groceryName, () => navigation.navigate("GroceryList"))}>
+					<FontAwesome style={styles.headerIcons} name="check" size={25}/>
+				</TouchableOpacity>
+			</View>
+		),
+		title: "Add Grocery Item",
+		headerStyle: {
+			backgroundColor: "#4854C7",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontWeight: "bold",
+		}
 	}
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	headerIcons: {
+		marginRight: 10,
+		color: "#fff"
+	}
+});
 
 export default GroceryCreateScreen;
