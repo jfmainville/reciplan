4146f55a2f0211283f99e727fe058a697a5f3ab2
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-13 11:51:31 -0400

    Add the useLayoutEffect method to the GroceryCreateScreen.js screen
    Add the useLayoutEffect method to the GroceryCreateScreen.js screen.

--- app/src/screens/GroceryCreateScreen.js
+++ app/src/screens/GroceryCreateScreen.js
@@ -1,17 +1,29 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useLayoutEffect } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import GroceryForm from "../components/GroceryForm";
+import { useTheme } from 'react-native-paper';
 import { Context as GroceryContext } from "../context/GroceryContext";
 import { FontAwesome } from "@expo/vector-icons";
 
-const GroceryCreateScreen = ({ navigation }) => {
+const GroceryCreateScreen = ({ navigation, route }) => {
+	const { headerButtonColor } = useTheme()
 	const { createGrocery } = useContext(GroceryContext);
 
-	useEffect(() => {
-		navigation.setParams({
-			createGrocery
-		});
-	}, []);
+	useLayoutEffect(() => {
+		const groceryQuantity = route.params ? route.params.groceryQuantity : ""
+		const groceryName = route.params ? route.params.groceryName : ""
+
+		navigation.setOptions({
+			headerRight: () => (
+				<View style={{ flexDirection: "row" }}>
+					<TouchableOpacity
+						onPress={() => createGrocery(groceryQuantity, groceryName, () => navigation.navigate("GroceryList"))}>
+						<FontAwesome name="check" size={25} style={{ color: headerButtonColor }}/>
+					</TouchableOpacity>
+				</View>
+			)
+		})
+	})
 
 	return (
 		<GroceryForm
