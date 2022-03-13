c743049f3240c509e5c825ff009695ba98f8d9ff
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-13 11:59:53 -0400

    Remove the NavLink component from the SigninScreen.js screen
    Remove the NavLink component from the SigninScreen.js screen.

--- app/src/screens/SigninScreen.js
+++ app/src/screens/SigninScreen.js
@@ -1,8 +1,6 @@
 import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
-import { NavigationEvents } from "react-navigation";
 import AuthForm from "../components/AuthForm";
-import NavLink from "../components/NavLink";
 import { Context as AuthContext } from "../context/AuthContext";
 
 const SigninScreen = () => {
@@ -10,18 +8,11 @@ const SigninScreen = () => {
 
 	return (
 		<View style={styles.container}>
-			<NavigationEvents
-				onWillFocus={clearErrorMessage}
-			/>
 			<AuthForm
 				headerText="Sign In to Your Account"
 				submitButtonText="Sign In"
 				errorMessage={state.errorMessage}
 				onSubmit={signin}
-			/>
-			<NavLink
-				text="Don't have an account? Sign up instead"
-				routeName="Signup"
 			/>
 		</View>
 	);
