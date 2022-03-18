fe395e68abed30cf97d1d496831a13d0e853b78d
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-18 15:27:09 -0400

    Update the styling of the SigninScreen.js screen
    Update the styling of the SigninScreen.js screen.

--- app/src/screens/SigninScreen.js
+++ app/src/screens/SigninScreen.js
@@ -1,29 +1,44 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import AuthForm from "../components/AuthForm";
 import { Context as AuthContext } from "../context/AuthContext";
+import { Button, useTheme } from "react-native-paper";
 
-const SigninScreen = () => {
+const SigninScreen = ({ navigation }) => {
+	const { colors } = useTheme()
 	const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
+	useEffect(() => {
+		clearErrorMessage()
+	}, []);
+
 	return (
-		<View style={styles.container}>
+		<View style={{ flex: 1, justifyContent: "center", backgroundColor: colors.primary }}>
 			<AuthForm
-				headerText="Sign In to Your Account"
+				headerText="Sign In"
 				submitButtonText="Sign In"
 				errorMessage={state.errorMessage}
 				onSubmit={signin}
 			/>
+			<Button
+				style={{
+					marginLeft: 5,
+					marginRight: 5,
+					marginTop: 10,
+					borderColor: "white",
+					borderWidth: 1,
+					backgroundColor: "white"
+				}}
+				labelStyle={{ fontSize: 25, color: colors.primary }}
+				mode="outlined"
+				onPress={() => navigation.navigate("Signup")}
+			>
+				Sign Up
+			</Button>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		marginBottom: 250
-	}
-});
+const styles = StyleSheet.create({});
 
 export default SigninScreen;
