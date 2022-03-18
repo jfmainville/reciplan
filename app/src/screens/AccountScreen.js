bd887a5db6ef768f8814d41e7646a01095bc622e
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-18 15:25:21 -0400

    Refactor AccountScreen.js screen to use the react-native-paper package
    Refactor the AccountScreen.js screen to use the react-native-paper package.

--- app/src/screens/AccountScreen.js
+++ app/src/screens/AccountScreen.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome } from "@expo/vector-icons";
-import { Button } from "react-native-elements";
+import { Button } from "react-native-paper";
 import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
@@ -12,11 +12,15 @@ const AccountScreen = () => {
 		<SafeAreaView forceInset={{ top: "always" }}>
 			<Text style={{ fontSize: 48 }}>AccountScreen</Text>
 			<Button
-				title="Sign Out" onPress={signout}
-			/>
+				onPress={signout}
+			>
+				Sign out
+			</Button>
 			<Button
-				title="DeleteAccount" onPress={deleteAccount}
-			/>
+				onPress={deleteAccount}
+			>
+				Delete account
+			</Button>
 		</SafeAreaView>
 	);
 };
