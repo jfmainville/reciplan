ef9d8b30e0a87d67dd8798c16f28c351f7ce7819
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-03-18 15:27:20 -0400

    Update the styling of the SignupScreen.js screen
    Update the styling of the SignupScreen.js screen.

--- app/src/screens/SignupScreen.js
+++ app/src/screens/SignupScreen.js
@@ -1,24 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
-import { NavigationEvents } from "react-navigation";
 import AuthForm from "../components/AuthForm";
+import { Button, useTheme } from "react-native-paper";
 
 const SignupScreen = ({ navigation }) => {
+	const { colors } = useTheme()
 	const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
+	useEffect(() => {
+		clearErrorMessage()
+	}, []);
+
 	return (
-		<View style={styles.container}>
-			<NavigationEvents
-				onWillFocus={clearErrorMessage}
-			/>
+		<View style={{ flex: 1, justifyContent: "center", backgroundColor: colors.primary }}>
 			<AuthForm
-				headerText="Sign Up for Tracker"
+				headerText="Sign Up"
 				submitButtonText="Sign Up"
 				errorMessage={state.errorMessage}
 				onSubmit={signup}
 			/>
-
+			<Button
+				style={{
+					marginLeft: 5,
+					marginRight: 5,
+					marginTop: 10,
+					borderColor: "white",
+					borderWidth: 1,
+					backgroundColor: "white"
+				}}
+				labelStyle={{ fontSize: 25, color: colors.primary }}
+				mode="outlined"
+				onPress={() => navigation.navigate("Signin")}
+			>
+				Sign In
+			</Button>
 		</View>
 	);
 };
