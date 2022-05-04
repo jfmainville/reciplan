97d200db1ee7a7c2a0fa14e7cc3819fe0f6794e2
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-05-03 22:06:06 -0400

    Remove the Text component from the module imports as it's no longer used
    Remove the Text component from the module imports as it's no longer used.

--- app/src/screens/AccountScreen.js
+++ app/src/screens/AccountScreen.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome } from "@expo/vector-icons";
 import { Button } from "react-native-paper";
