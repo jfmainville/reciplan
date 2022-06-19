330a74e3915ba5a02890b00d4ebde696a808e616
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-06-19 14:34:28 -0400

    Add the RecipePlannerScreen.js screen to the project
    Add the RecipePlannerScreen.js screen to the project.

--- app/src/screens/RecipePlannerScreen.js
+++ app/src/screens/RecipePlannerScreen.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Calendar, Agenda } from "react-native-calendars";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 
 const RecipePlannerScreen = () => {
+	const { colors } = useTheme()
+	const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+	const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+	const workout = { key: 'workout', color: 'green' };
 
 	return (
 		<View>
-			<Text>RecipePlannerScreen</Text>
+			<Calendar
+				theme={{
+					arrowColor: colors.primary,
+					todayTextColor: colors.primary
+				}}
+				markingType={'multi-dot'}
+				markedDates={{
+					'2022-04-20': { dots: [vacation, massage, workout], selected: true },
+					'2017-10-24': { dots: [massage, workout], disabled: true }
+				}}
+			/>
+			<Agenda/>
 		</View>
 	);
 };
