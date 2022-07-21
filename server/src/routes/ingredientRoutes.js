3189aa7a1c24af26a867de014d36e645b4aacaff
Jean-Frederic Mainville <jfmainville@outlook.com>
2022-07-20 21:32:35 -0400

    Refactor deepl REST API calls to use the deeplTranslate function
    Refactor the deepl REST API calls to use the deeplTranslate function. This makes the code easier to read as the logic is centrally managed through the deeplTranslate function.

--- server/src/routes/ingredientRoutes.js
+++ server/src/routes/ingredientRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const pluralize = require("pluralize");
-const axios = require("axios");
+const deeplTranslate = require("../configs/deepl")
 const requireAuth = require("../middlewares/requireAuth");
 
 const Ingredient = mongoose.model("Ingredient");
@@ -12,69 +12,66 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.post("/ingredients/create", async (req, res) => {
-        let { ingredientName } = req.body;
-        ingredientName = ingredientName.toLowerCase()
+		let { ingredientName } = req.body;
+		ingredientName = ingredientName.toLowerCase()
 
-        if (!ingredientName) {
-            return res.status(422).send({ error: "You must provide all the recipe information to continue" });
-        }
+		if (!ingredientName) {
+			return res.status(422).send({ error: "You must provide all the recipe information to continue" });
+		}
 
-        // Extract the translation for each ingredient
-        const deeplTranslateUrl = `${process.env.DEEPL_URL}/v2/translate?auth_key=${process.env.DEEPL_API_KEY}`
-        const ingredientNameSource = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&source_lang=FR&target_lang=EN`)
+		// Extract the translation for each ingredient
+		const ingredientNameSource = await deeplTranslate(ingredientName, "FR", "EN")
 
-        try {
-            Ingredient.findOne({ name: ingredientNameSource["data"]["translations"][0]["text"] }, async (err, doc) => {
-                if (!doc) {
-                    if (ingredientName === ingredientNameSource["data"]["translations"][0]["text"]) {
-                        const { data } = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&source_lang=EN&target_lang=FR`)
-
-                        const ingredient = new Ingredient({
-                            name: ingredientName,
-                            translations: [
-                                {
-                                    singular: ingredientName,
-                                    plural: pluralize(ingredientName),
-                                    languageCode: "EN"
-                                },
-                                {
-                                    singular: data["translations"][0]["text"],
-                                    plural: pluralize(data["translations"][0]["text"]),
-                                    languageCode: "FR"
-                                }
-                            ]
-                        })
-                        await ingredient.save();
-                        res.send(ingredient);
-                    } else {
-                        const { data } = await axios.post(deeplTranslateUrl + `&text=${ingredientName}&target_lang=EN`)
-
-                        const ingredient = new Ingredient({
-                            name: data["translations"][0]["text"],
-                            translations: [
-                                {
-                                    singular: data["translations"][0]["text"],
-                                    plural: pluralize(data["translations"][0]["text"]),
-                                    languageCode: "EN"
-                                },
-                                {
-                                    singular: ingredientName,
-                                    plural: pluralize(ingredientName),
-                                    languageCode: "FR"
-                                }
-                            ]
-                        })
-                        await ingredient.save();
-                        res.send(ingredient);
-                    }
-                } else {
-                    res.send(doc)
-                }
-            })
-        } catch (err) {
-            return res.status(422).send({ error: err.message });
-        }
-    }
+		try {
+			Ingredient.findOne({ name: ingredientNameSource["data"]["translations"][0]["text"] }, async (err, doc) => {
+				if (!doc) {
+					if (ingredientName === ingredientNameSource["data"]["translations"][0]["text"]) {
+						const { data } = await deeplTranslate(ingredientName, "EN", "FR")
+						const ingredient = new Ingredient({
+							name: ingredientName,
+							translations: [
+								{
+									singular: ingredientName,
+									plural: pluralize(ingredientName),
+									languageCode: "EN"
+								},
+								{
+									singular: data["translations"][0]["text"],
+									plural: pluralize(data["translations"][0]["text"]),
+									languageCode: "FR"
+								}
+							]
+						})
+						await ingredient.save();
+						res.send(ingredient);
+					} else {
+						const { data } = await deeplTranslate(ingredientName, null, "EN")
+						const ingredient = new Ingredient({
+							name: data["translations"][0]["text"],
+							translations: [
+								{
+									singular: data["translations"][0]["text"],
+									plural: pluralize(data["translations"][0]["text"]),
+									languageCode: "EN"
+								},
+								{
+									singular: ingredientName,
+									plural: pluralize(ingredientName),
+									languageCode: "FR"
+								}
+							]
+						})
+						await ingredient.save();
+						res.send(ingredient);
+					}
+				} else {
+					res.send(doc)
+				}
+			})
+		} catch (err) {
+			return res.status(422).send({ error: err.message });
+		}
+	}
 )
 
 module.exports = router;
